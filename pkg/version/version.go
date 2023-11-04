5fa274931c267b906d3f83a8d17c4af6b5a5e3b2
pincher95 <yuri.tsuprun@gmail.com>
2023-11-03 20:46:11 -0400

    update git2consul version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -26,7 +26,7 @@ var (
 	Branch    string
 	BuildDate string
 	GitSHA1   string
-	Version   = "v0.1.2-dev"
+	Version   = "v0.1.3-dev"
 )
 
 // Print writes application version details to standard output.
